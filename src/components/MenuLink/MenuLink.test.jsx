b485bed107a1d7af0fadf39aa5543689ae750ca8
AlexMarquesAlves <atanaelleonardo@gmail.com>
2021-12-16 00:37:11 -0300

    Componente MenuLink com testes

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { MenuLink } from '.';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(
+      <MenuLink link={'https://www.google.com.br/'}>Children</MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should open in newTab', () => {
+    renderTheme(
+      <MenuLink link={'https://www.google.com.br/'} newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should match snapshot', () => {
+    renderTheme(
+      <MenuLink link={'https://www.google.com.br/'} newTab={false}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toMatchSnapshot();
+  });
+});
