d9bea3655b05748df9f353027141ccffc835ec4a
AlexMarquesAlves <atanaelleonardo@gmail.com>
2021-12-15 23:46:47 -0300

    Testes TextComponent

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -1,10 +1,14 @@
-// import { screen } from '@testing-library/react';
-// import { renderTheme } from '../../styles/render-theme';
-// import { TextComponent } from '.';
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { TextComponent } from '.';
 
-// describe('<TextComponent />', () => {
-//   it('should render', () => {
-//     renderTheme(<TextComponent>Children</TextComponent>);
-//     expect(screen.getByRole('heading')).toBeInTheDocument();
-//   });
-// });
+describe('<TextComponent />', () => {
+  it('should render a text', () => {
+    renderTheme(<TextComponent>Children</TextComponent>);
+    expect(screen.getByText('Children')).toBeInTheDocument();
+  });
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
