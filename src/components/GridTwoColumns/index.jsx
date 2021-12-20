0431d64d472dae5c822c5aed87ec7ac258190bfa
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-20 15:38:21 -0300

    Grid Two Columns

--- src/components/GridTwoColumns/index.jsx
+++ src/components/GridTwoColumns/index.jsx
@@ -1,14 +1,28 @@
 import P from 'prop-types';
 import * as Styled from './styles';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
 
-export const GridTwoColumns = ({ children }) => {
+export const GridTwoColum = ({ title, text, srcImg, background = false }) => {
   return (
-    <Styled.Container>
-      <h1>{children}</h1>
-    </Styled.Container>
+    <SectionBackground background={background}>
+      <Styled.Container background={background}>
+        <Styled.TextContainer>
+          <Heading uppercase>{title}</Heading>
+          <TextComponent>{text}</TextComponent>
+        </Styled.TextContainer>
+        <Styled.ImageContainer>
+          <Styled.Image src={srcImg} alt={title} />
+        </Styled.ImageContainer>
+      </Styled.Container>
+    </SectionBackground>
   );
 };
 
-GridTwoColumns.propTypes = {
-  children: P.node.isRequired,
+GridTwoColum.propTypes = {
+  title: P.string.isRequired,
+  text: P.string.isRequired,
+  srcImg: P.string.isRequired,
+  background: P.bool,
 };
--- src/components/GridTwoColumns/stories.jsx
+++ src/components/GridTwoColumns/stories.jsx
@@ -1,10 +1,12 @@
-import { GridTwoColumns } from '.';
+import { GridTwoColum } from '.';
 
 export default {
-  title: 'GridTwoColumns',
-  component: GridTwoColumns,
+  title: 'GridTwoColum',
+  component: GridTwoColum,
   args: {
-    children: 'GridTwoColumns',
+    title: 'Grid two columns',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio magnam culpa eveniet doloribus harum? Ipsam, a necessitatibus? Sequi sunt accusantium quod, animi iure a, aliquid dolor ea vel magni dolore?`,
+    srcImg: 'assets/images/javascript.svg',
   },
   argTypes: {
     children: { type: 'string' },
@@ -14,7 +16,7 @@ export default {
 export const Template = (args) => {
   return (
     <div>
-      <GridTwoColumns {...args} />
+      <GridTwoColum {...args} />
     </div>
   );
 };
