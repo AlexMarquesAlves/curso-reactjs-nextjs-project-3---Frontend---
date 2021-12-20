59afeb94746818c86414617d89b51e0b42189c36
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-20 17:06:03 -0300

    Criando um template base - Parte 1

---
+++ src/templates/Base/stories.jsx
@@ -0,0 +1,33 @@
+import { Base } from '.';
+
+import mock from './mock';
+import { GridText } from '../../components/GridText';
+import gridMock from '../../components/GridText/mock';
+
+export default {
+  title: 'Template/Base',
+  component: Base,
+  args: {
+    children: (
+      <>
+        <GridText {...gridMock} background />
+        <GridText {...gridMock} />
+        <GridText {...gridMock} background />
+        <GridText {...gridMock} />
+        <GridText {...gridMock} background />
+      </>
+    ),
+    ...mock,
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <Base {...args} />
+    </div>
+  );
+};
