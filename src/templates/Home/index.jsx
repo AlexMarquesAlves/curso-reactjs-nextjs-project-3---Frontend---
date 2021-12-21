67bbba1cdc28ed05b35d28e4bafcf21432502840
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-21 16:22:19 -0300

    Loading

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -5,6 +5,7 @@ import { mockBase } from '../Base/mock';
 import * as Styled from './styles';
 import { mapData } from '../../api/map-data';
 import { PageNotFound } from '../PageNotFoud';
+import { Loading } from '../Loading';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -31,7 +32,7 @@ function Home() {
   }
 
   if (data && !data.slug) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
   return <Base {...mockBase} />;
