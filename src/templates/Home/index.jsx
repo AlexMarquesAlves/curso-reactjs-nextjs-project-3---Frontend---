4144fc68f965489ad77db09adc9256f215399273
Alex Maques Alves <atanaelleonardo@gmail.com>
2021-12-21 16:14:55 -0300

    Carregando os dados da API e error 404

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Heading } from '../../components/Heading';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 import * as Styled from './styles';
+import { mapData } from '../../api/map-data';
+import { PageNotFound } from '../PageNotFoud';
 
 function Home() {
-  return (
-    <div className="Home">
-      <Base {...mockBase}></Base>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetch(
+          'http://localhost:1337/pages/?slug=landing-page',
+        );
+        const json = await data.json();
+        const pageData = mapData(json);
+        setData(pageData[0]);
+      } catch (e) {
+        setData(undefined);
+      }
+    };
+
+    load();
+  }, []);
+
+  if (data === undefined) {
+    return <PageNotFound />;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>;
+  }
+
+  return <Base {...mockBase} />;
 }
 
 export default Home;
